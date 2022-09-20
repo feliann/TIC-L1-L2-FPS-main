314e8f8ed6b36052fbc50fdf317ab1edb12a8e1d
feliann <101577329+feliann@users.noreply.github.com>
2022-09-20 09:33:27 -0300

    Initial commit

---
+++ Assets/COLLIDER.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class COLLIDER : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Piza")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+            SceneManager.LoadScene("Video");
+            Time.timeScale = 1f;
+
+        }
+    }
+}
---
+++ Assets/buscadorElementoEn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buscadorElementoEn : MonoBehaviour
+{
+    [SerializeField] GameObject[] arrayDeMesas;
+    [SerializeField] string tagDeMesa;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrayDeMesas = GameObject.FindGameObjectsWithTag(tagDeMesa);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+    void BuscarElementoEnArray()
+    {
+        //para buscar elemento usamos un while
+        bool isFound = false;
+        int i = 0;
+        while (i < arrayDeMesas.Length && !isFound)
+        {
+            if (arrayDeMesas[i].GetComponent<mesa>().owner=="felipe")
+            {
+                Destroy(arrayDeMesas[i]);
+                return;
+            }
+            i++;
+        }
+    }    
+   
+}
